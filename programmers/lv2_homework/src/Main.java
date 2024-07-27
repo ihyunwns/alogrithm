66a703726c4938a786d70a0531968c43e2e34278
ihyunwns <ihyunwns@gmail.com>
2024-07-27 17:10:13 +0900

    프로그래머스 lv2 과제 완료하기 문제 해결

--- programmers/lv2_homework/src/Main.java
+++ programmers/lv2_homework/src/Main.java
@@ -3,16 +3,8 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
-        String[][] plans = new String[][]{{"korean", "11:40", "20"}, {"english", "12:10", "30"},
+        String[][] plans = new String[][]{{"korean", "11:40", "20"}, {"english", "12:10", "20"},
                                           {"math", "12:30", "40"}};
-
-        // 이떄 null 값이 반환됨 왜 그럴까?
-        // 현재는 과제 완료 시점에 현재 시각과 과제의 시작 시간이 같다면 다음 과제를 스택에 넣는데
-        // 예를 들어서 과제 완료가 된 시점에도 아직 다음 과제 시작할 시간이 되지 않았다면 넣지 못함
-        // 이 경우를 고려해야 하고
-        // try-catch 문을 이용하지 않고 한번 만들어봤으면 좋겠음
-        // 즉, 지금은 다음 과제에 대한 부분을 stack의 사이즈를 이용해서 찾고 있는데 제일 마지막 과제이거나 혼자 남은 과제일경우는 OutBounds 에러가 뜨기 때문에
-        // 이를 어떻게 해결할 지 고민해야함 / 1. stack.size를 그대로 대입하지 말고 int next = stack.size() 로 하되 stack.size()가 plan의 size보다 클 경우 next를 보정하는 방법
 
         System.out.println(Arrays.toString(s.solution(plans)));
     }
@@ -73,52 +65,61 @@ class Plan implements Comparable<Plan>{
 class Solution {
     public String[] solution(String[][] plans) {
         String[] answer = new String[plans.length];
-        int n = 0;
         Stack<Plan> stack = new Stack<>();
-
         List<Plan> plan = new ArrayList<>();
 
-        int i = 0;
+        //Plan 시작 시간 별로 정렬
         for (String[] p : plans) {
             plan.add(new Plan(p[0], p[1], Integer.parseInt(p[2])));
         }
         Collections.sort(plan);
 
-        Plan top;
+        int n = 0;
+        Plan current_plan = plan.get(0);
         stack.add(plan.get(0));
         int current_time = plan.get(0).getStart();
 
-        while (!stack.isEmpty()) {
-            top = stack.peek();
+        while (!plan.isEmpty()) {
 
-            try {
-                if (plan.get(stack.size()).getStart() < current_time + top.getRemainTime()) {
+            int current_index = stack.size() -1;
+            int next_index = stack.size();
+            current_plan = stack.peek(); //현재 진행 중인 과제
 
-                    top.addTaken_time(plan.get(stack.size()).getStart() - current_time);
-                    current_time += plan.get(stack.size()).getStart() - current_time;
+            // 다음 진행할 과제가 있으면서 현재 시간 + 현재 과제 소요 시간이 다음 과제 시작 시간을 넘을 때
+            if (next_index < plan.size() && current_time + current_plan.getRemainTime() > plan.get(next_index).getStart()) {
 
-                    stack.add(plan.get(stack.size()));
-                } else {
-                    current_time += top.getRemainTime();
-                    top.addTaken_time(top.getRemainTime());
-                }
-            } catch (IndexOutOfBoundsException ignored) {
-                current_time += top.getRemainTime();
-                top.addTaken_time(top.getRemainTime());
-            }
+                int progress = plan.get(next_index).getStart() - current_time; //진행할 시간
 
-            //현재 과제의 소요된 시간이 과제 소요 시간과 같아진다면 해당 과제는 과제 완료
-            if (top.getTaken_time() >= top.getPlaytime()) {
-                plan.remove(stack.size()-1);
+                current_plan.addTaken_time(progress); // 현재 과제를 진행할 시간 만큼 진행 시켜줌
+                current_time += progress; // 현재 시간을 진행한 시간 만큼 진행 시킴
+                stack.push(plan.get(next_index)); // 두번째 과제를 stack에 넣어 줌
+
+                // 다음 진행할 과제가 없거나 현재 시간 + 첫번째 과제 소요 시간이 두번째 과제 시작 시간을 넘지 않거나 같을 때
+            } else {
+                //현재 과제를 끝까지 진행 시켜야 함
+                int remain_time = current_plan.getRemainTime(); //현재 과제의 남은 시간
+                current_plan.addTaken_time(remain_time); // 남은 시간 만큼 현재 과제 진행
+
+                current_time += remain_time; // 현재 시간을 남은 시간 만큼 진행
+
+                // 스택에서 빼는 것과 동시에 answer에 해당 과제의 이름을 넣어줌
                 answer[n++] = stack.pop().getName();
-                try {
-                    //새로운 과제가 들어올 시간이면 새로운 과제를 stack에 넣어준다.
-                    if (current_time == plan.get(stack.size()).getStart()) {
-                        stack.push(plan.get(stack.size()));
-                    }
-                } catch (IndexOutOfBoundsException ignored) {}
-            }
+                plan.remove(current_index); // plan에서도 빼줌
 
+                current_index = stack.size()-1;
+                next_index = stack.size();
+
+                // 다음 과제가 있다면
+                if (next_index < plan.size()) {
+                    // 진행중이던 과제가 없다면
+                    if (stack.isEmpty()) {
+                        // 그 과제가 진행 되는 시간으로 현재 시간을 바꿔주고 스택에 넣어줌
+                        stack.push(plan.get(next_index));
+                        int progress = plan.get(next_index).getStart() - current_time;
+                        current_time += progress;
+                    }
+                }
+            }
         }
 
         return answer;
